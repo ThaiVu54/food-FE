e99c5337ba6b77fcf7f30ab32c22db9a3a8c77b9
ThaiVu54 <thai01654369440@gmail.com>
2022-12-20 17:59:18 +0700

    loi anh

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  //API LOCAL
+  API_LOCAL: 'http://localhost:8080/',
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBp-ebweCRWNTPzdQOg6wFQxOLu5iSf4dA',
+    authDomain: 'ecommerce-5f9fe.firebaseapp.com',
+    projectId: 'ecommerce-5f9fe',
+    storageBucket: 'ecommerce-5f9fe.appspot.com',
+    messagingSenderId: '1022972919809',
+    appId: '1:1022972919809:web:d20a6cf4bd62c4bbd00000',
+    measurementId: 'G-4LNBSXRX9Y'
+  },
+};
